bb546fceca63ade3ed872bbb7f94d6c81f069651
macarena88 <ereina784t@fpdrioja.com>
2024-10-27 22:04:17 +0100

    summit11

---
+++ js/macros.js
@@ -0,0 +1,13 @@
+// macros.js
+
+// Macro para cambiar el color de fondo
+function cambiarFondo(color) {
+    document.body.style.backgroundColor = color;
+}
+
+// Macro para mostrar un mensaje
+function mostrarMensaje(mensaje) {
+    alert(mensaje);
+}
+
+// Puedes crear más macros según tus necesidades
