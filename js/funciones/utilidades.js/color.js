b45701a5802c03ad8c7dc626a56e50c176ff95c6
macarena88 <ereina784t@fpdrioja.com>
2024-10-27 18:11:44 +0100

    commit

---
+++ js/funciones/utilidades.js/color.js
@@ -0,0 +1,4 @@
+function getRandomColor() {
+    const colors = ["#FFC107", "#FF5722", "#8E24AA", "#1976D2", "#4CAF50"];
+    return colors[Math.floor(Math.random() * colors.length)];
+}
---
+++ js/main.js/backgr.js
@@ -0,0 +1,3 @@
+document.getElementById("colorButton").addEventListener("click", function() {
+    this.style.backgroundColor = getRandomColor();
+});
